c9eafd4cede9eca51a163dcc001a155acc254748
Marco Junior <tombombadil182@gmail.com>
2018-07-18 16:20:10 -0300

    Creating the workspace, setting the testspace and installing the mongoDB

---
+++ app.spec.js
@@ -0,0 +1,11 @@
+const assert = require('assert')
+
+describe('BookService.js', () => {
+ it('Meu primeiro teste!', () => {
+   const a = 1
+   const b = 2
+   const c = a + b
+
+   assert.equal(c, 3)
+ })
+})
